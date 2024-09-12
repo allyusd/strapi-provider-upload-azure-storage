7c13d51aba345ed431b965efb9b7c07af5864d2a
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2024-09-12 16:38:21 +0800

    ci: publish a user-scoped package

--- src/index.ts
+++ src/index.ts
@@ -138,8 +138,8 @@ async function handleUpload(
 
     await client.uploadStream(
         file.stream,
-        (config.uploadOptions || uploadOptions).bufferSize,
-        (config.uploadOptions || uploadOptions).maxBuffers,
+        config.uploadOptions?.bufferSize || uploadOptions?.bufferSize,
+        config.uploadOptions?.maxBuffers || uploadOptions?.maxBuffers,
         options
     );
 }
